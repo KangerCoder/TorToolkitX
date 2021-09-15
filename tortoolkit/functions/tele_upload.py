f4eba6f9b1270fccc04b5ca3ff547da9070afb1d
Yash Oswal <yashoswal18@gmail.com>
2021-09-15 07:27:23 +0000

    Update tele_upload.py

--- tortoolkit/functions/tele_upload.py
+++ tortoolkit/functions/tele_upload.py
@@ -285,11 +285,13 @@ async def upload_a_file(
         user_msg = await message.get_reply_message()
 
     # todo improve this uploading ✔️
-    file_name = os.path.basename(path)
+    prefix = get_val("PREFIX")
+    os.rename(path,f'{os.path.dirname(path)}/{prefix} {os.path.basename(path)}')
+    path = f'{os.path.dirname(path)}/{prefix} {os.path.basename(path)}'
+    file_name = ""
+    file_name += os.path.basename(path)
     caption_str = ""
-    caption_str += "<code>"
     caption_str += file_name
-    caption_str += "</code>"
     metadata = extractMetadata(createParser(path))
 
     if metadata is not None:
@@ -489,11 +491,13 @@ async def upload_single_file(
 
     queue = message.client.exqueue
 
-    file_name = os.path.basename(path)
+    prefix = get_val("PREFIX")
+    os.rename(path,f'{os.path.dirname(path)}/{prefix} {os.path.basename(path)}')
+    path = f'{os.path.dirname(path)}/{prefix} {os.path.basename(path)}'
+    file_name = ""
+    file_name += os.path.basename(path)
     caption_str = ""
-    caption_str += "<code>"
     caption_str += file_name
-    caption_str += "</code>"
 
     if user_msg is None:
         user_msg = await message.get_reply_message()
