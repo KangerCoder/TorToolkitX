523e3aab81adc34d50e83ca655bf7b473ad83db1
yashoswalyo <yashoswal18@gmail.com>
2021-10-18 13:04:41 +0530

    trr

--- tortoolkit/functions/tele_upload.py
+++ tortoolkit/functions/tele_upload.py
@@ -342,7 +342,7 @@ async def upload_a_file(
 				output_file_name = os.path.basename(path)
 				new_path = path
 				if output_file_name.rsplit(".",maxsplit=1)[1] != "mkv":
-					output_file_name = output_file_name.rsplit(".")[0]+".mkv"
+					output_file_name = output_file_name.rsplit(".",maxsplit=1)[0]+".mkv"
 					new_path = f"{os.path.dirname(path)}/{output_file_name}"
 				subprocess.call(f"ffmpeg -hide_banner -loglevel -y -i '{path}' -c copy -attach '{thumb_path}' -metadata:s:t mimetype=image/jpeg -map 0 '{output_file_name}' ",shell=True)
 				logging.info("Running ffmpeg")
@@ -558,7 +558,7 @@ async def upload_single_file(
 				output_file_name = os.path.basename(path)
 				new_path = path
 				if output_file_name.rsplit(".",maxsplit=1)[1] != "mkv":
-					output_file_name = output_file_name.rsplit(".")[0]+".mkv"
+					output_file_name = output_file_name.rsplit(".",maxsplit=1)[0]+".mkv"
 					new_path = f"{os.path.dirname(path)}/{output_file_name}"
 				subprocess.call(f"ffmpeg -hide_banner -loglevel error -y -i '{path}' -c copy -attach '{thumb_image_path}' -metadata:s:t mimetype=image/jpeg -map 0 '{output_file_name}' ",shell=True)
 				logging.info(f"Running ffmpeg -hide_banner -loglevel error -y -i '{path}' -c copy -attach '{thumb_image_path}' -metadata:s:t mimetype=image/jpeg -map 0 '{output_file_name}' ")
